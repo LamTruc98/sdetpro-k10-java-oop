b3ea6e03db926ee14e71bf6949aff5204e499e55
LamTruc <truclam8398@gmail.com>
2022-09-16 00:25:24 +0700

    lab3_1

---
+++ src/lesson_03/Lab3_1.java
@@ -0,0 +1,23 @@
+package lesson_03;
+
+public class Lab3_1 {
+    public static void main(String[] args) {
+        int[] intArr = {1, 2, 3, 4, 5};
+        int even_count = 0;
+        int odd_count = 0;
+
+        /*check each element in the array
+         *if its odd, increment odd count
+         *else, increment even count
+         */
+        for (int i = 0; i < intArr.length; i++) {
+            if (intArr[i] %2== 1) {
+                odd_count++;
+
+            } else
+                even_count++;
+        }
+        System.out.println("Even numders:" + even_count);
+        System.out.println("Odd numders:" + odd_count);
+    }
+}
