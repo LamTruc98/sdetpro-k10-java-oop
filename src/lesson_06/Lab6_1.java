bd88dfeb979b8c4edae14fc49ad1b62fd3cfca15
LamTruc <truclam8398@gmail.com>
2022-09-27 13:57:55 +0700

    update lab06

--- src/lesson_06/Lab6_1.java
+++ src/lesson_06/Lab6_1.java
@@ -1,2 +1,16 @@
-package lesson_06;public class Lab6_1 {
+package lesson_06;
+
+public class Lab6_1 {
+    public static void main(String[] args) {
+        String value = "2hrs and 5 minutes";
+        String[] resultList = value.split(" and");
+        int totalMinutes = 0;
+        if(resultList.length == 4) {
+            totalMinutes = Integer.parseInt(resultList[0]) * 60 + Integer.parseInt(resultList[2]);
+        } else {
+            totalMinutes = Integer.parseInt(resultList[0]);
+        }
+        System.out.println(totalMinutes);
+
+    }
 }
