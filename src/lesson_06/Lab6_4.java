01e037adccf56ef1f4b472496b6b29983f28bb41
LamTruc <truclam8398@gmail.com>
2022-09-28 01:08:11 +0700

    update lab6_4

--- src/lesson_06/Lab6_4.java
+++ src/lesson_06/Lab6_4.java
@@ -1,12 +1,22 @@
 package lesson_06;
 
 
+import java.util.Scanner;
+
 public class Lab6_4 {
     //String url = "https://google.com";
     //Check http OR https; domain name, .com OR .net
     public static void main(String[] args) {
-        String url = "https://google.com";
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your URL: ");
+        String url = sc.nextLine();
+        String protocol = url.substring(0, url.indexOf(":"));
+        String topLevelDomain = url.split("\\.")[1];
+        String subDomain = url.replace("." + topLevelDomain, "");
+        subDomain = subDomain.replace(protocol + "://", "");
+        System.out.println("Your protocol  is: " + protocol);
+        System.out.println("Your subdomain is: " + subDomain);
+        System.out.println("Your top-level domain is: ." + topLevelDomain);
 
     }
 }
